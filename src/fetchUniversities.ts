4a0abc70e7628b9cd874ba744810b05ca5958e9e
shutonakatsubo <125226255+shutonakatsubo@users.noreply.github.com>
2023-11-16 14:01:55 -0500

    wrote a comment

--- src/fetchUniversities.ts
+++ src/fetchUniversities.ts
@@ -1,6 +1,6 @@
 import fetch from "../include/fetch.js";
 
 export function fetchUniversities(query: string): Promise<string[]> {
-  // TODO
+  //Hi from Shuto
   return new Promise(res => res([]));
 }
