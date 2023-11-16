bf27a201200af80af82e56f4dc83b7f38f56df75
zhaochen426 <124215308+zhaochen426@users.noreply.github.com>
2023-11-16 14:31:51 -0500

    Not complatly correct, still need to consider type - jack

--- src/fetchCurrentTemperature.ts
+++ src/fetchCurrentTemperature.ts
@@ -8,5 +8,29 @@ interface TemperatureReading {
 
 export function fetchCurrentTemperature(coords: GeoCoord): Promise<TemperatureReading> {
   // TODO
-  return new Promise(res => res({ time: [], temperature_2m: [] }));
+  const base = 'https://220.maxkuechen.com/currentTemperature/forecast'
+  const url = new URL(base)
+  url.searchParams.append('latitude', coords.lat.toString())
+  url.searchParams.append('longitude', coords.lon.toString())
+  url.searchParams.append('hourly', 'temperature_2m')
+  url.searchParams.append('temperature_unit', 'fahrenheit')
+  // https://220.maxkuechen.com/currentTemperature/forecast?latitude=___&longitude=___&hourly=temperature_2m&temperature_unit=fahrenheit
+  return fetch(url.toString())
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Error! Status: ${response.status}`)
+      }
+      return response.json()
+    })
+    .then((data: TemperatureReading) => {
+      return {
+        time: data.time,
+        temperature_2m: data.temperature_2m
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching the url', error)
+      throw error
+    })
 }
+// return new Promise(res => res({ time: [], temperature_2m: [] }));
