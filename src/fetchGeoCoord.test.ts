ae88d5a1cfc234ee6be72c815bf9d138111ae03d
zhaochen426 <124215308+zhaochen426@users.noreply.github.com>
2023-11-16 14:32:33 -0500

    jack

--- src/fetchGeoCoord.test.ts
+++ src/fetchGeoCoord.test.ts
@@ -10,6 +10,8 @@ describe("fetchGeoCoord", () => {
       assert(typeof result.lon === "number"); // Assert that the lon value is a number
       assert(typeof result.lat === "number"); // Assert that the lat value is a number
       assert(Object.keys(result).length === 2); // Assert there are only two keys in the object
+      assert(result.lat === 42.3869382)
+      assert(result.lon === -72.52991477067445)
     });
   });
 });
